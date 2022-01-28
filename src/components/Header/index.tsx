d7279c5eee11d3d97a55bf8ab433c4ee3cdafa65
Pietro Dessotti <pdessotti@direct.one>
2022-01-27 22:11:42 -0300

    feat: testing eslint.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,27 +8,27 @@ import styles from './styles.module.scss';
 
 export const Header = () => {
 
-    const data  = useContext(CartContext);
+    const data = useContext(CartContext);
 
     return (
-    <header className={styles.headerContainer}>
-        <div className={styles.headerContent}>
-           {/* Maneira antiga de consumir um contexto
-            <CartContext.Consumer> 
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                {/* Maneira antiga de consumir um contexto
+                <CartContext.Consumer> 
                 {(data) => {
                 console.log(data)
                 return <p>Testando data da forma antiga </p>
                 }}
             </ CartContext.Consumer>
              */}
-            <Image src='/images/logotipo-ethereum.png' alt="Logotipo do CryptoChange" 
-             width={40} height={40}/>
-             <a href="">CryptoChange</a>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Exchenge</a>
-            </nav>
-        </div>        
-    </header>
+                <Image src='/images/logotipo-ethereum.png' alt="Logotipo do CryptoChange"
+                    width={40} height={40} />
+                <a href="">CryptoChange</a>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Exchenge</a>
+                </nav>
+            </div>
+        </header>
     )
 }
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -2,20 +2,20 @@ import Image from "next/image"
 import { useEffect } from "react"
 import { api } from "../../services/api"
 
-export const Home = ():JSX.Element => {
+export const Home = (): JSX.Element => {
 
-    
+
     return (
-    <header className={styles.headerContainer}>
-        <div className={styles.headerContent}>
-     <Image src='/images/logotipo-ethereum.png' alt="Logotipo do CryptoChange"          width={40} height={40}/>
-             <a href="">CryptoChange</a>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Exchenge</a>
-            </nav>
-        </div>        
-    </header>
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image src='/images/logotipo-ethereum.png' alt="Logotipo do CryptoChange" width={40} height={40} />
+                <a href="">CryptoChange</a>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Exchenge</a>
+                </nav>
+            </div>
+        </header>
     )
 }
 
