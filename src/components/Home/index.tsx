1b6b0c6a8a333f774ebdd2550d7228a6722d7fd8
Pietro Dessotti <pdessotti@direct.one>
2022-01-27 22:03:18 -0300

    feat: testing husky.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,8 +8,7 @@ export const Home = ():JSX.Element => {
     return (
     <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
-            <Image src='/images/logotipo-ethereum.png' alt="Logotipo do CryptoChange" 
-             width={40} height={40}/>
+     <Image src='/images/logotipo-ethereum.png' alt="Logotipo do CryptoChange"          width={40} height={40}/>
              <a href="">CryptoChange</a>
             <nav>
                 <a href="">Home</a>
